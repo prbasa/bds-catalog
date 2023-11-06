b50a9dea5ce34e3251543c5cf1ff34c2071c2ea0
Paulo Roberto <prbasa@gmail.com>
2023-11-06 07:46:08 -0300

    Ultimo update de user

--- frontweb/src/pages/Admin/Auth/Login/index.tsx
+++ frontweb/src/pages/Admin/Auth/Login/index.tsx
@@ -2,9 +2,8 @@ import { Link } from 'react-router-dom';
 import ButtonIcon from 'components/ButtonIcon';
 import './styles.css';
 import { useForm } from 'react-hook-form';
-import { requestBackendLogin } from 'util/requests';
+import { requestBackendLogin, saveAuthData } from 'util/requests';
 import { useState } from 'react';
-
 
 type FormData = {
   username: string;
@@ -23,6 +22,7 @@ const Login = () => {
   const onSubmit = (formData: FormData) => {
     requestBackendLogin(formData)
       .then((response) => {
+        saveAuthData(response.data);
         setHasError(false);
         console.log('SUCESSO', response);
       })
@@ -33,62 +33,60 @@ const Login = () => {
   };
 
   return (
-    <div>
-     
-     
-      <div className="base-card login-card">
-        <h1>LOGIN</h1>
-        {hasError && (
-          <div className="alert alert-danger">
-            Erro ao tentar efetuar o login
+    <div className="base-card login-card">
+      <h1>LOGIN</h1>
+      {hasError && (
+        <div className="alert alert-danger">Erro ao tentar efetuar o login</div>
+      )}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="mb-4">
+          <input
+            {...register('username', {
+              required: 'Campo obrigatório',
+              pattern: {
+                value: /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/,
+                message: 'Email inválido'
+              }
+            })}
+            type="text"
+            className={`form-control base-input ${errors.username ? 'is-invalid' : ''} `}
+            placeholder="Username"
+            name="username"
+          />
+          <div className="invalid-feedback d-block">
+            {errors.username?.message}
           </div>
-        )}
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <div className="mb-4">
-            <input
-              {...register('username', {
-                required: 'Campo obrigatório',
-              })}
-              type="text"
-              className="form-control base-input"
-              placeholder="Username"
-              name="username"
-            />
-            <div className="invalid-feedback d-block">
-              {errors.username?.message}
-            </div>
-          </div>
+        </div>
 
-          <div className="mb-2">
-            <input
-              {...register('password', {
-                required: 'Campo obrigatório',
-              })}
-              type="password"
-              className="form-control base-input"
-              placeholder="Password"
-              name="password"
-            />
-            <div className="invalid-feedback d-block">
-              {errors.password?.message}
-            </div>
+        <div className="mb-2">
+          <input
+            {...register('password', {
+              required: 'Campo obrigatório',
+            })}
+            type="password"
+            className={`form-control base-input ${errors.password ? 'is-invalid' : ''} `}
+            placeholder="Password"
+            name="password"
+          />
+          <div className="invalid-feedback d-block">
+            {errors.password?.message}
           </div>
-          <Link to="/admin/auth/recover" className="login-link-recover">
-            Esqueci a senha
+        </div>
+        <Link to="/admin/auth/recover" className="login-link-recover">
+          Esqueci a senha
+        </Link>
+        <div className="login-submit">
+          <center>
+            <ButtonIcon text="Fazer login" />
+          </center>
+        </div>
+        <div className="signup-container">
+          <span className="not-registered">Não tem Cadastro?</span>
+          <Link to="/admin/auth/register" className="login-link-register">
+            CADASTRAR
           </Link>
-          <div className="login-submit">
-            <center>
-              <ButtonIcon text="Fazer login" />
-            </center>
-          </div>
-          <div className="signup-container">
-            <span className="not-registered">Não tem Cadastro?</span>
-            <Link to="/admin/auth/register" className="login-link-register">
-              CADASTRAR
-            </Link>
-          </div>
-        </form>
-      </div>
+        </div>
+      </form>
     </div>
   );
 };
--- frontweb/src/util/requests.ts
+++ frontweb/src/util/requests.ts
@@ -1,8 +1,19 @@
 import qs from 'qs';
 
-import axios from 'axios';
+import axios, { AxiosRequestConfig } from 'axios';
+
+type LoginResponse = {
+    access_token: string;
+    token_type : string;
+    expires_in : number;
+    scope : string;
+    useFirstName: string;
+    userId: number;
+}
 
 export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+
+const tokenKey = 'authData';
 
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? 'dscatalog';
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET ?? 'dscatalog123';
@@ -29,4 +40,18 @@ export const requestBackendLogin = (loginData : LoginData) => {
 
     return axios({method: 'POST', baseURL: BASE_URL, url: '/oauth/token', data, headers});
 
+}
+
+export const requestBackend = (config: AxiosRequestConfig) => {
+    return axios(config);
+}
+
+export const saveAuthData = (obj : LoginResponse) => {
+    localStorage.setItem(tokenKey, JSON.stringify(obj));
+}
+
+export const getAuthData = (obj : LoginResponse) => {
+    const str = localStorage.getItem(tokenKey) ?? "{}";
+    return JSON.parse(str) as LoginResponse;
+    
 }
